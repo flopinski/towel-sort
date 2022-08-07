d20f279bbd46b609838e8952c808c2c2cc4e0b5d
flopinski <103384762+flopinski@users.noreply.github.com>
2022-08-07 16:34:31 +0200

    feat: done with this

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 
-// You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let result = [];
+  if (matrix === undefined) return []
+  if (matrix === []) return 'empty array'
+  else {
+
+    for (let i = 0; i < matrix.length; i++) {
+      for (let j = 0; j < matrix[i].length; j++) {
+        let columnIdx = i % 2 === 0
+        ? j
+        : matrix[i].length - j - 1;
+        result.push(matrix[i][columnIdx])
+      } 
+    }
+   return result 
+  }
+  
+  
 }
